84cf038c3da54c29b14b3664291152b873b70fdc
jonghyeon99 <twjh99@naver.com>
2024-04-18 06:18:06 +0000

    few modify retriever

--- retriever.py
+++ retriever.py
@@ -24,28 +24,29 @@ class TextRetriever:
                     vectorlist.append(text[0])
         return vectorlist
     
-    # 2000글자씩 자르고 500 글자가 겹침
-    def split_documents(self, documents):
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=500)
-        splits = text_splitter.split_documents(documents)
+    # 1000글자씩 자르고 100 글자가 겹침
+    def split_documents(self, vectorlist):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+        splits = text_splitter.split_documents(vectorlist)
         return splits
     
     # split 해놓은 텍스트 데이터를 embedding한 VectorDB를 생성
     def create_retriever(self, splits):
-        vectordb = Chroma.from_documents(documents=splits,
-                                          embedding=GoogleGenerativeAIEmbeddings(model="models/embedding-001"))        
-        retriever = vectordb.as_retriever(k=2)
+        vectordb = Chroma.from_documents(splits,
+                                          embedding=GoogleGenerativeAIEmbeddings(model="models/embedding-001", temperature=0))        
+        retriever = vectordb.as_retriever(k=1)
         return retriever
     
     def search(self, input_prompt, retriever):
-        response = retriever.invoke(input=input_prompt)
+        #response = retriever.invoke(input_prompt)
+        response = retriever.get_relevant_documents(input_prompt)
         return response
 
 # 사용 예시:
 if __name__ == "__main__":
     retriever = TextRetriever()
-    documents = retriever.load_documents_from_csv('./joseon.csv')
-    splits = retriever.split_documents(documents)
+    vectorlist = retriever.load_documents_from_csv('./joseon.csv')
+    splits = retriever.split_documents(vectorlist)
     text_retriever = retriever.create_retriever(splits)
     input_prompt = input("User: ")
     response = retriever.search(input_prompt, text_retriever)
