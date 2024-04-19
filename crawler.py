2a0ecb7294a44dd60343d7880b64677fcb6a8c5f
jonghyeon99 <twjh99@naver.com>
2024-04-19 04:15:33 +0000

    modify crawler

--- crawler.py
+++ crawler.py
@@ -1,20 +1,11 @@
-import csv
-
 class Crawler:
     def __init__(self, url):
         self.url = url
 
     def urllist(self, csv_file):
-        urls = []
         with open(csv_file, 'r') as f:
-            rdr = csv.reader(f)
-            for line in rdr:
-                url = line[0]
-                urls.append(Crawler(url))
-        return urls
+            return f.read().split(',')
+
 if __name__ == "__main__":   
     url_obj = Crawler('')
     url_list = url_obj.urllist('./joseon.csv')
-
-for url_link in url_list:
-    print(url_link.url)
