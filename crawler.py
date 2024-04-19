c3589918abf80c0a5ae06c67eb9d718f0c2927a3
jonghyeon99 <twjh99@naver.com>
2024-04-19 06:40:40 +0000

    modify crawler.py

--- crawler.py
+++ crawler.py
@@ -1,11 +1,4 @@
 class Crawler:
-    def __init__(self, url):
-        self.url = url
-
     def urllist(self, csv_file):
         with open(csv_file, 'r') as f:
             return f.read().split(',')
-
-if __name__ == "__main__":   
-    url_obj = Crawler('')
-    url_list = url_obj.urllist('./joseon.csv')
